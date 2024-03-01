f02bef319def08e1aed6c616f7ff487e24cb8e2c
Ewhan <ewhan.han@gmail.com>
2024-02-29 19:00:02 -0500

    Update nvim plugins

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add tsx and treesitter
+    vim.list_extend(opts.ensure_installed, {
+      "tsx",
+      "typescript",
+      "html",
+      "css",
+      "markdown",
+      "javascript",
+      "json",
+      "lua",
+      "regex",
+      "yaml",
+      "toml",
+      "python",
+    })
+  end,
+}
