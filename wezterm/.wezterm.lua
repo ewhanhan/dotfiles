23bac48780a9cfa53209592ac34aa9970c81bbd8
Ewhan Han <40779221+ewhanhan@users.noreply.github.com>
2024-08-14 21:19:45 -0400

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,42 +1,5 @@
 local wezterm = require("wezterm")
-
-local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == 'true'
-end
-
-local direction_keys = {
-  Left = 'h',
-  Down = 'j',
-  Up = 'k',
-  Right = 'l',
-  -- reverse lookup
-  h = 'Left',
-  j = 'Down',
-  k = 'Up',
-  l = 'Right',
-}
-
-local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
-    action = wezterm.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
-end
+local act = wezterm.action
 
 local config = {}
 if wezterm.config_builder then
@@ -47,7 +10,10 @@ config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font =
     wezterm.font_with_fallback(
       {
-        { family = "JetBrains Mono", italic = false }
+        {
+          family = "JetBrains Mono",
+          italic = false
+        }
       }
     )
 
@@ -57,7 +23,10 @@ config.font_rules = {
     italic = true,
     font = wezterm.font_with_fallback(
       {
-        { family = "JetBrains Mono", style = "Normal" }
+        {
+          family = "JetBrains Mono",
+          style = "Normal"
+        }
       }
     )
   },
@@ -66,7 +35,10 @@ config.font_rules = {
     italic = true,
     font = wezterm.font_with_fallback(
       {
-        { family = "JetBrains Mono", italic = false }
+        {
+          family = "JetBrains Mono",
+          italic = false
+        }
       }
     )
   }
@@ -84,42 +56,65 @@ config.window_padding = {
 }
 config.hide_tab_bar_if_only_one_tab = true
 
-config.leader = { key = 'w', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = {
+  key = 'w',
+  mods = 'CTRL',
+  timeout_milliseconds = 1000
+}
 config.keys = {
   {
     key = '|',
     mods = 'LEADER',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    action = act.SplitHorizontal {
+      domain = 'CurrentPaneDomain'
+    },
   },
   {
     key = '-',
     mods = 'LEADER',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    action = act.SplitVertical {
+      domain = 'CurrentPaneDomain'
+    },
   },
   {
     key = 'a',
     mods = 'LEADER|CTRL',
-    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+    action = act.SendKey {
+      key = 'a',
+      mods = 'CTRL'
+    },
   },
   {
     key = 'w',
     mods = 'CMD',
-    action = wezterm.action.DisableDefaultAssignment,
+    action = act.DisableDefaultAssignment,
   },
   {
     key = 'w',
     mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
+    action = act.CloseCurrentPane {
+      confirm = false
+    },
   },
-  split_nav('move', 'h'),
-  split_nav('move', 'j'),
-  split_nav('move', 'k'),
-  split_nav('move', 'l'),
-  -- resize panes
-  split_nav('resize', 'h'),
-  split_nav('resize', 'j'),
-  split_nav('resize', 'k'),
-  split_nav('resize', 'l'),
+  {
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = act.ClearScrollback 'ScrollbackOnly',
+  },
+  -- Clears the scrollback and viewport leaving the prompt line the new first line.
+  {
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  },
+  {
+    mods = "CMD",
+    key = "Backspace",
+    action = act.SendKey({
+      mods = "CTRL",
+      key = "u"
+    })
+  },
 }
 
 return config
