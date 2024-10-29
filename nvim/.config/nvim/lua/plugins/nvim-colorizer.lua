8fb5a2092ad919be09fdd196650c0c78ee1c51a2
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-10-29 17:13:25 -0400

    Add nvim-colorizer

---
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,12 @@
+-- A high-performance color highlighter for Neovim which has no external dependencies!
+
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+}
