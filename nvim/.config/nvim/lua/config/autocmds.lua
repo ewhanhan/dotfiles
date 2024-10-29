a03a7c9188dbb9e588381b5c0fea752a05e9b398
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-10-29 17:12:31 -0400

    Add disable cmp for markdown files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,22 @@
--- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+-- Disable nvim-cmp for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    local cmp = require("cmp")
+    -- completely disable
+    -- require("cmp").setup.buffer({ enabled = false })
+
+    -- manual complete
+    cmp.setup({
+      completion = {
+        autocomplete = false,
+        completeopt = "menu,menuone,noinsert",
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.complete(),
+      }),
+    })
+  end,
+})
