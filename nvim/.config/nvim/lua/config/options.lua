5346a75052daf4658c1a6e27911c11a451521fb3
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-10-29 17:13:15 -0400

    Update nvim options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 if vim.env.VSCODE then
   vim.g.vscode = true
 end
+
+local opt = vim.opt
+local o = vim.o
+
+opt.swap = false
+opt.incsearch = true
+
+o.incsearch = true -- starts searching as soon as typing, without enter needed
+o.ignorecase = true -- ignore letter case when searching
+o.smartcase = true -- case insentive unless capitals used in searcher
