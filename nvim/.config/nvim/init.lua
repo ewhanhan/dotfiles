23a20d0ce7cd0d5284d6f365914515736e20694b
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-10-30 09:48:17 -0400

    Refactor init.lua and remove redundant autocmds for Markdown files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 if vim.env.VSCODE then
   vim.g.vscode = true
 end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
