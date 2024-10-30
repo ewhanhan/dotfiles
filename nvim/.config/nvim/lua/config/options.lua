d8e4ca297e2c1a133f7ce52150ba92d1a84b3fa6
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-10-29 23:33:17 -0400

    Add support for VSCode integration in LazyVim configuration

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-if vim.env.VSCODE then
-  vim.g.vscode = true
-end
-
 local opt = vim.opt
 local o = vim.o
 
