c2c8848c67413fd4ceb09e34521749774d83c17c
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2024-11-18 12:16:25 -0500

    test

--- nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-if vim.env.VSCODE then
-  vim.g.vscode = true
-end
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- nvim/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,1 +0,0 @@
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
