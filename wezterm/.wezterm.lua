6b165bd289db12e511fd2a94ddf4a40e37976cea
ewhanhan <40779221+ewhanhan@users.noreply.github.com>
2025-06-14 23:27:09 -0400

    Refactor wezterm configuration for clarity and add new keybindings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,9 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 local on_mac = wezterm.target_triple == "aarch64-apple-darwin"
-local config = {}
-
+local config = wezterm.config_builder and wezterm.config_builder() or {}
+config.automatically_reload_config = true
 config.term = "wezterm"
-
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
 
 config.default_prog = {
 	"/bin/zsh",
@@ -39,25 +35,15 @@ config.inactive_pane_hsb = {
 	saturation = 0.6,
 	brightness = 0.6,
 }
+config.font = wezterm.font_with_fallback({
+	"Hasklug Nerd Font Mono", -- Your preferred font
+	"JetBrains Mono", -- Fallback 1
+	"Menlo", -- macOS system fallback
+})
 
-config.font_size = on_mac and 14 or 20
+config.scrollback_lines = 1000000
+config.font_size = on_mac and 14 or 16
 config.line_height = on_mac and 1.2 or 1.25
-config.font = wezterm.font("Hasklug Nerd Font Mono", { weight = "Medium" })
-config.font_rules = {
-	{
-		intensity = "Bold",
-		font = wezterm.font("Hasklug Nerd Font Mono", { weight = "DemiBold" }),
-	},
-	{
-		intensity = "Bold",
-		italic = true,
-		font = wezterm.font("Hasklug Nerd Font Mono", { weight = "DemiBold", style = "Italic" }),
-	},
-	{
-		italic = true,
-		font = wezterm.font("JetBrainsMono Nerd Font", { style = "Italic" }),
-	},
-}
 
 config.keys = {
 	{ key = "a", mods = "CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
@@ -93,6 +79,27 @@ config.keys = {
 			wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
 			wezterm.action.SendKey({ key = "x" }),
 		}),
+	},
+	{
+		key = "d",
+		mods = "CMD",
+		action = act.Multiple({
+			act.SendKey({ key = "a", mods = "CTRL" }),
+			act.SendKey({ key = "-" }),
+		}),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = act.Multiple({
+			act.SendKey({ key = "a", mods = "CTRL" }),
+			act.SendKey({ key = "\\" }),
+		}),
+	},
+	{
+		key = "/",
+		mods = "CTRL",
+		action = act.Search({ CaseInSensitiveString = "" }),
 	},
 }
 
